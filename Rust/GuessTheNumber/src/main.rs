8a1a6c1d80e034d82168950121e0cecbc61131bd
Azrood <53349565+Azrood@users.noreply.github.com>
2020-10-24 01:51:06 +0000

    Rust game (#1)
    * add guess the number in rust

---
+++ Rust/GuessTheNumber/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number! (Between 1 and 100)");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+    let mut attempts: u64 = 0;
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        attempts += 1;
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number)
+        {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! You found the secret number in {} attempts !", attempts);
+                break;
+            }
+        }
+    }
+}
